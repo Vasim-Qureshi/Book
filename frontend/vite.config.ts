b6c47f83779f4f53708e4386372a5c9aa75d7ee1
Vasim Qureshi <vasimqureshi2000@gmail.com>
2026-09-09 22:35:41 +0530

    Update (vite.config.ts) for disabled of consol.log()

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,10 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
-})
+
+  esbuild: {
+    drop: mode === 'production' ? ['debugger'] : [],
+  },
+}))
